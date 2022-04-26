3fd6386d99670006c0522dcf38f03f7882f16b25
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-04-26 22:29:33 +0400

    initial commit

--- components/Loading.js
+++ components/Loading.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Loading() {
   return (
-    <div className="text-white">Loading</div>
+    <div className="flex h-screen w-screen  items-center justify-center">
+      <div className="w-[100px] h-[100px] animate-spin border-[4px] border-red-700 relative rounded-full ">
+        <div className="bg-black absolute top-0 left-0 w-[40px] rounded-full h-[40px]"></div>
+      </div>
+    </div>
   )
 }
 
